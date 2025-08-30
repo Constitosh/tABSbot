8324193f6bebca3029b6fa5bb311063816a024cf
Mfer Vestside <97671389+Constitosh@users.noreply.github.com>
2025-08-30 11:39:23 +0200

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,6 +1,23 @@
 module.exports = {
   apps: [
-    { name: 'tabs-bot', script: 'src/bot.js', watch: false, env: { NODE_ENV: 'production' } },
-    { name: 'tabs-worker', script: 'src/refreshWorker.js', watch: false, env: { NODE_ENV: 'production' } }
+    {
+      name: 'tabs-worker',
+      script: 'src/refreshWorker.js',
+      cwd: '/root/tABSbot/tABSbot',
+      args: '',                 // add '--cron' if you want DEFAULT_TOKENS auto-refresh
+      watch: false,
+      autorestart: true,
+      max_memory_restart: '300M',
+      env: { NODE_ENV: 'production' }
+    },
+    {
+      name: 'tabs-bot',
+      script: 'src/bot.js',
+      cwd: '/root/tABSbot/tABSbot',
+      watch: false,
+      autorestart: true,
+      max_memory_restart: '300M',
+      env: { NODE_ENV: 'production' }
+    }
   ]
 };
