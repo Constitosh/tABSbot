ec20873313141896814a881c8973b77cb963d47a
Mfer Vestside <97671389+Constitosh@users.noreply.github.com>
2025-09-12 10:22:52 +0200

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -11,8 +11,8 @@ import { refreshPnl } from './pnlWorker.js'; // ⬅ only refreshPnl to avoid exp
 import { renderPNL } from './renderers_pnl.js';
 
 import { getIndexSnapshot, buildIndexSnapshot } from './indexer.js';
-import { renderIndex } from './renderers_index.js';
-import { refreshIndex } from './indexWorker.js';
+import { ensureIndexSnapshot } from './indexWorker.js';
+import { renderIndexView } from './renderers_index.js';
 
 // --- Bot with longer handler timeout + global error catcher ---
 const bot = new Telegraf(process.env.BOT_TOKEN, { handlerTimeout: 30_000 });
