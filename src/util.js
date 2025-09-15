94dd3d8b298a869d7caacdbcc1813023a2d1f726
Mfer Vestside <97671389+Constitosh@users.noreply.github.com>
2025-09-15 16:54:45 +0200

    Update util.js

--- src/util.js
+++ src/util.js
@@ -1,6 +1,34 @@
-export const sleep = (ms) => new Promise(r => setTimeout(r, ms));
-export const isAddress = (s='') => /^0x[a-fA-F0-9]{40}$/.test(s);
-export const shortAddr = a => a ? a.slice(0,6)+'…'+a.slice(-4) : '';
-export const pct = n => (n ?? 0).toFixed(2) + '%';
-export const num = (n, d=2) => Number(n ?? 0).toLocaleString(undefined, { maximumFractionDigits: d });
-export const now = () => Math.floor(Date.now()/1000);
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function isAddress(address) {
+  return /^0x[a-fA-F0-9]{40}$/.test(address);
+}
+
+function shortAddr(addr) {
+  return addr ? `${addr.slice(0, 6)}...${addr.slice(-4)}` : 'N/A';
+}
+
+function num(n) {
+  return n.toLocaleString(undefined, { maximumFractionDigits: 0 });
+}
+
+function escapeMarkdownV2(text) {
+  return text.replace(/([_*[\]()~`>#+\-=|{}.!\\])/g, '\\$1');
+}
+
+function isKnownContract(addr) {
+  // Expand with chain-specific routers/LPs
+  const known = [
+    '0x7a250d5630b4cf539739df2c5dacb4c659f2488d', // Uniswap V2 Router
+    '0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45'  // Uniswap V3 Router
+  ];
+  return known.some(k => k.toLowerCase() === addr.toLowerCase());
+}
+
+function now() {
+  return Date.now();
+}
+
+module.exports = { sleep, isAddress, shortAddr, num, escapeMarkdownV2, isKnownContract, now };
