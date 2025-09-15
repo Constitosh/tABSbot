6e5510c5ff28b15ed11fcfaac7a7ec6553f229e1
Mfer Vestside <97671389+Constitosh@users.noreply.github.com>
2025-09-15 20:22:03 +0200

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,19 +1,19 @@
-// ecosystem.config.cjs
 module.exports = {
   apps: [
     {
       name: 'tabs-bot',
-      cwd: '/root/tABSbot/tABSbot',
       script: 'src/bot.js',
-      node_args: '--enable-source-maps',
-      env: { NODE_ENV: 'production' }
+      env: {
+        NODE_ENV: 'production'
+      }
     },
     {
       name: 'tabs-worker',
-      cwd: '/root/tABSbot/tABSbot',
       script: 'src/refreshWorker.js',
-      node_args: '--enable-source-maps',
-      env: { NODE_ENV: 'production' }
+      env: {
+        NODE_ENV: 'production',
+        CRON: 'true'
+      }
     }
   ]
 };
