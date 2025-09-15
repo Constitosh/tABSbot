53c0ea477a739dea6f16646b3df59074b20fa827
Mfer Vestside <97671389+Constitosh@users.noreply.github.com>
2025-09-15 17:02:56 +0200

    Update util.js

--- src/util.js
+++ src/util.js
@@ -19,10 +19,9 @@ function escapeMarkdownV2(text) {
 }
 
 function isKnownContract(addr) {
-  // Expand with chain-specific routers/LPs
   const known = [
-    '0x7a250d5630b4cf539739df2c5dacb4c659f2488d', // Uniswap V2 Router
-    '0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45'  // Uniswap V3 Router
+    '0x7a250d5630b4cf539739df2c5dacb4c659f2488d',
+    '0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45'
   ];
   return known.some(k => k.toLowerCase() === addr.toLowerCase());
 }
@@ -31,4 +30,4 @@ function now() {
   return Date.now();
 }
 
-module.exports = { sleep, isAddress, shortAddr, num, escapeMarkdownV2, isKnownContract, now };
+export { sleep, isAddress, shortAddr, num, escapeMarkdownV2, isKnownContract, now };
